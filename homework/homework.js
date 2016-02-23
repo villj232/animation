700bff1650f762c739e7027e51234ea98fc117b2
villj232 <javiervillegas@javiers-MacBook-Pro.local>
2016-02-23 15:04:08 -0500

    finished

--- homework/homework.js
+++ homework/homework.js
@@ -11,5 +11,33 @@ function setup(){
 }
 
 function draw(){
-  //draw
+
+  background ("white");
+
+
+
+  if(mouseX < 412){
+    fill ("yellow");
+  } else if (mouseX < 1000){
+    fill ("orange");
+  } else if(mouseX < 1300){
+    fill ("red");
+  } else{
+    fill ("yellow");
+  }
+
+
+
+  if(mouseX < windowWidth/3){
+    background("#FBEC4A");
+  } if (mouseX > windowWidth/3){
+    background ("#F0A421")
+
+  }
+
+  if (mouseIsPressed) {
+background ("black");
+  }
+
+ellipse(200,200,200,200)
 }
